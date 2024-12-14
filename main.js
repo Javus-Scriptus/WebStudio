af39471b0556b59103cbbf3bd50f367ba67ff87d
Igor Krasylnikov <garikkrass@gmail.com>
2024-12-14 14:54:29 +0100

    added burger icon + js code

--- main.js
+++ main.js
@@ -27,21 +27,5 @@
 // burger menu
 document.querySelector('.burger').addEventListener('click', function () {
   this.classList.toggle('active');
-  document.querySelector('.header-nav').classList.toggle('open');
+  document.querySelector('.header').classList.toggle('open');
 });
-/* (() => {
-              const refs = {
-                  openModalBtn: document.querySelector("[data-modal-open]"),
-                  closeModalBtn: document.querySelector("[data-modal-close]"),
-                  modal: document.querySelector("[data-modal]"),
-              };
-
-              refs.openModalBtn.addEventListener("click", toggleModal);
-              refs.closeModalBtn.addEventListener("click", toggleModal);
-
-              function toggleModal() {
-                  refs.modal.classList.toggle("is-open")
-                  refs.modal.classList.toggle("is-hidden");
-                  document.body.classList.toggle("no-scroll");
-              }
-          })(); */
