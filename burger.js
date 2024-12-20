e48fbbe425f7b54b71a8a6914963a046c7c6d884
Igor Krasylnikov <garikkrass@gmail.com>
2024-12-20 16:28:20 +0100

    @media moved to every style files

--- burger.js
+++ burger.js
@@ -1,4 +1,10 @@
 document.querySelector('.burger').addEventListener('click', function () {
   this.classList.toggle('active');
   document.querySelector('.header').classList.toggle('open');
+
+  if (document.body.classList.contains('no-scroll')) {
+    document.body.classList.remove('no-scroll');
+  } else {
+    document.body.classList.add('no-scroll');
+  }
 });
